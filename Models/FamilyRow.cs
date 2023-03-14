7be310900c95d464bca6b3dc9f0b294973cbbb77
Jeffrey Hoover <jeffreyhoover1@gmail.com>
2023-03-13 21:25:32 -0400

    start family page

---
+++ Models/FamilyRow.cs
@@ -0,0 +1,9 @@
+﻿namespace MyAncestry.Models;
+
+public class FamilyRow
+{
+    public string FatherName { get; set; }
+    public string MotherName { get; set; }
+    public string Location { get; set; }
+    public DateTime DateTime { get; set; }
+}
